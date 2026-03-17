651eeb39da1cbabc07188d9b81d32bea50c21779
Nedim38090 <nedimdjellali11@gmail.com>
2026-03-17 11:14:29 +0100

    fix db

--- db.php
+++ db.php
@@ -14,9 +14,13 @@ function getDB() {
     }
 }
 
+function estConnecte() {
+    return isset($_SESSION['user']['id']);
+}
 
-
-
+function estAdmin() {
+    return (isset($_SESSION['user']['role']) && $_SESSION['user']['role'] === 'administrateur');
+}
 
 function encryptData($data) {
     $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
